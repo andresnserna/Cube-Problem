7869a8863b02a771e32f41a13c8197c6fbbdbe55
andresnserna <andresnserna@icloud.com>
2025-12-02 01:19:26 -0600

    finsihed cubenetImpl, hashcode subject to change

--- cosc3327_cubeProblem/src/model/CubeNetImpl_Skeleton.java
+++ cosc3327_cubeProblem/src/model/CubeNetImpl_Skeleton.java
@@ -38,7 +38,15 @@ public class CubeNetImpl_Skeleton implements CubeNet
 	//contract" is satisfied
 	public int hashCode()
 	{
-		throw new RuntimeException("NOT IMPLEMENTED YET!");
+		int result = 13; //taylor's favorite number
+		result = 27 * result + colorCode(faceToColor.get(Face.TOP));
+	    result = 27 * result + colorCode(faceToColor.get(Face.LEFT));
+	    result = 27 * result + colorCode(faceToColor.get(Face.FRONT));
+	    result = 27 * result + colorCode(faceToColor.get(Face.RIGHT));
+	    result = 27 * result + colorCode(faceToColor.get(Face.BOTTOM));
+	    result = 27 * result + colorCode(faceToColor.get(Face.BACK));
+	    
+	    return result;
 	}
 	
 	//Improving on the inherited equals() is mandatory
@@ -48,12 +56,48 @@ public class CubeNetImpl_Skeleton implements CubeNet
 	//contract" is satisfied
 	public boolean equals(Object obj)
 	{
-		throw new RuntimeException("NOT IMPLEMENTED YET!");
+		// Check if null
+	    if (obj == null) {
+	        return false;
+	    }
+	    
+	    // Check if same instance
+	    if (this == obj) {
+	        return true;
+	    }
+	    
+	    // Check if same type
+	    if (!(obj instanceof CubeNet)) {
+	        return false;
+	    }
+	    
+	    // Cast and compare face by face
+	    CubeNet other = (CubeNet) obj;
+	    
+	    return this.getColor(Face.TOP).equals(other.getColor(Face.TOP)) &&
+	           this.getColor(Face.LEFT).equals(other.getColor(Face.LEFT)) &&
+	           this.getColor(Face.FRONT).equals(other.getColor(Face.FRONT)) &&
+	           this.getColor(Face.RIGHT).equals(other.getColor(Face.RIGHT)) &&
+	           this.getColor(Face.BOTTOM).equals(other.getColor(Face.BOTTOM)) &&
+	           this.getColor(Face.BACK).equals(other.getColor(Face.BACK));
 	}
 	
 	//Implement a decent toString() for yourself during development
 	public String toString()
 	{
-		throw new RuntimeException("NOT IMPLEMENTED YET!");
+	    return CubeNetRendererImpl_Serna.CUBENET_UNICODE_SQUARE_RENDERER.getRendering(this);
+	}
+	
+	private int colorCode(Color color)
+	{
+	    if (color == null) return 0;
+	    if (color.equals(Color.RED)) return 1;
+	    if (color.equals(Color.GREEN)) return 2;
+	    if (color.equals(Color.BLUE)) return 3;
+	    if (color.equals(Color.YELLOW)) return 4;
+	    if (color.equals(Color.ORANGE)) return 5;
+	    if (color.equals(Color.BLACK)) return 6;
+	    if (color.equals(Color.WHITE)) return 7;
+	    return 0; // fallback for unsupported colors
 	}
 }
